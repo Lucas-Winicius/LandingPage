662f9bfc5646e133210b5d11333a85817eaf5cdf
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-06-20 20:13:26 -0300

    att

--- paoBot/script/setaconfig.js
+++ paoBot/script/setaconfig.js
@@ -1,7 +1,7 @@
 (function () {
     var seta = document.getElementById('seta'); 
     window.addEventListener('scroll', function () {
-        if (window.scrollY > 0) seta.classList.remove('setapause'); 
+        if (window.scrollY > 300) seta.classList.remove('setapause'); 
         else seta.classList.add('setapause');
     });
 })();
