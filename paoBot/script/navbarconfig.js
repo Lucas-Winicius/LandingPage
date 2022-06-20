b259706819eb581b89ab2a21879f6ea8e0156bb3
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-06-20 17:28:12 -0300

    navbar

---
+++ paoBot/script/navbarconfig.js
@@ -0,0 +1,9 @@
+(function () {
+    var menu = document.getElementById('menu'); 
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 0) menu.classList.add('menuFixo'); 
+        else menu.classList.remove('menuFixo');
+    });
+})();
+
+
