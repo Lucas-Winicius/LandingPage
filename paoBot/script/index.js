a0fe10f7ae9d2be91b226719c52527285615001c
Abobora <lucasclucas7@gmail.com>
2022-06-21 23:33:18 -0300

    Pagina de Login

--- paoBot/script/index.js
+++ paoBot/script/index.js
@@ -1,5 +1,25 @@
+let userName = 'Login'
+
 const botaoAdicionar = () => {
     window.open('https://discord.com/api/oauth2/authorize?client_id=965678250531643472&permissions=8&scope=bot%20applications.commands', '_blank')
+}
+
+// Pegar login
+const getLoggin = () => {
+    const inputElement = document.querySelector('#user')
+    if(inputElement.value == '') {
+        document.querySelector('#errorMessage').innerHTML = 'Insira um nome de usuario'
+    }
+    else {
+        userName = 
+        console.log(userName)
+        document.querySelector('body').id = inputElement.value
+        location.href = 'index.html'
+    }
+}
+
+const userLoad = () => {
+    document.querySelector('#userName').innerHTML = userName
 }
 
 // NavBarConfig
@@ -17,5 +37,6 @@ const seta = () => {
     else { seta.classList.add('setapause') }
 }
 
-// Ativa a funcao da seta e do nav
+// Ativa as funcoes
 window.addEventListener('scroll', function() {seta(), navBar()})
+window.addEventListener('load', userLoad())
