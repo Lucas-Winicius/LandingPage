47b4258c58242b5c436ded3aa6cc2110c438ca18
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-06-25 20:40:29 -0300

    Update loaderpag.js

--- paoBot/script/loaderpag.js
+++ paoBot/script/loaderpag.js
@@ -1,9 +1,8 @@
-
+//função de carregamento 
 var i = setInterval(function () {
-    
+
     clearInterval(i);
-  
-    // O código desejado é apenas isto:
+
     document.getElementById("loading").style.display = "none";
     document.getElementById("conteudo").style.display = "inline";
 
